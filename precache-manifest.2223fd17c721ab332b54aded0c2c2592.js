e4250124019831438052f8c5a7150a01a4c93c7d
Fahmi <fahmi.najjar@gmail.com>
2020-04-25 12:31:45 +0300

    First version

---
+++ precache-manifest.2223fd17c721ab332b54aded0c2c2592.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "785f51989450b4fdca5eb019a34a6056",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cd8646db61ff9c43d87",
+    "url": "/static/css/2.eabcd0b3.chunk.css"
+  },
+  {
+    "revision": "0632ec9a7d940c87f8ba",
+    "url": "/static/css/main.6d538c3b.chunk.css"
+  },
+  {
+    "revision": "5cd8646db61ff9c43d87",
+    "url": "/static/js/2.7a6c9ef4.chunk.js"
+  },
+  {
+    "revision": "0ccb314bec010f9b1c09c29cadb900cf",
+    "url": "/static/js/2.7a6c9ef4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0632ec9a7d940c87f8ba",
+    "url": "/static/js/main.f6db559a.chunk.js"
+  },
+  {
+    "revision": "de0e49a2f84db5e68c42",
+    "url": "/static/js/runtime-main.bc70d6df.js"
+  },
+  {
+    "revision": "30ed49af2f0e8409715e248ce764b0a9",
+    "url": "/static/media/Cairo-Black.30ed49af.ttf"
+  },
+  {
+    "revision": "a14c20325bb25764f63bcfbc076e9201",
+    "url": "/static/media/Cairo-Bold.a14c2032.ttf"
+  },
+  {
+    "revision": "a19b315dd2d7fcb3a379d9734a347088",
+    "url": "/static/media/Cairo-ExtraLight.a19b315d.ttf"
+  },
+  {
+    "revision": "b1d698b7e08cb6888f7822413532cc66",
+    "url": "/static/media/Cairo-Light.b1d698b7.ttf"
+  },
+  {
+    "revision": "e793edb57aaf4d59ac9e943cbf091937",
+    "url": "/static/media/Cairo-Regular.e793edb5.ttf"
+  },
+  {
+    "revision": "47622f2d4b9fbf054502c6b7c8680a16",
+    "url": "/static/media/Cairo-SemiBold.47622f2d.ttf"
+  },
+  {
+    "revision": "7c1a3c1617d7f42ccedec77078515e69",
+    "url": "/static/media/how-img.7c1a3c16.png"
+  },
+  {
+    "revision": "e891fabc096d4cfce2cad99025d09abc",
+    "url": "/static/media/icon-class.e891fabc.png"
+  },
+  {
+    "revision": "03ea5fe47757cbf0a15b8e47ff65642c",
+    "url": "/static/media/icon-students.03ea5fe4.png"
+  },
+  {
+    "revision": "3c3bf549efa282e52c1a9b5b9dca3b7b",
+    "url": "/static/media/icon-task.3c3bf549.png"
+  },
+  {
+    "revision": "e09452290f7b48955f221cd3098b21a5",
+    "url": "/static/media/logo.e0945229.png"
+  },
+  {
+    "revision": "966c878af516f144fa13887654646b09",
+    "url": "/static/media/student-img.966c878a.png"
+  },
+  {
+    "revision": "98e9abcbf2fcd5b130570871e77e3333",
+    "url": "/static/media/teacher-img.98e9abcb.png"
+  }
+]);
